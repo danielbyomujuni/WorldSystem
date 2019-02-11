21b30d489a4da09fa02ffd8ccc7588ece4b125a7
Butzlabben <darkstylerz@web.de>
2019-02-11 16:29:46 +0100

    Fixed autoupdate errpr

--- src/main/java/de/butzlabben/autoupdater/UpdateInformations.java
+++ src/main/java/de/butzlabben/autoupdater/UpdateInformations.java
@@ -34,6 +34,7 @@ public class UpdateInformations {
         try {
             URL url = new URL(URL);
             urlConn = url.openConnection();
+            urlConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
 
             if (urlConn != null)
                 urlConn.setReadTimeout(60 * 1000);
