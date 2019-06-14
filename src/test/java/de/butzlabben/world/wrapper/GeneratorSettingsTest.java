5411983f7daee0ed6294ed8f2df98af88543dce4
Butzlabben <darkstylerz@web.de>
2019-06-14 12:35:09 +0200

    Enhanced test

--- src/test/java/de/butzlabben/world/wrapper/GeneratorSettingsTest.java
+++ src/test/java/de/butzlabben/world/wrapper/GeneratorSettingsTest.java
@@ -3,7 +3,7 @@ package de.butzlabben.world.wrapper;
 import org.bukkit.WorldCreator;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class GeneratorSettingsTest {
 
@@ -11,5 +11,6 @@ public class GeneratorSettingsTest {
     public void asWorldCreator() {
         GeneratorSettings settings = new GeneratorSettings();
         WorldCreator creator = settings.asWorldCreator("test");
+        assertNotNull(creator);
     }
 }
