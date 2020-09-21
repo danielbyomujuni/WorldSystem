855dd288133559f861cd9917f705c85e3dee7373
Daniel <trainerlord@gmail.com>
2020-09-21 09:53:27 -0400

    WorldList File Completed
    TODO Convert Old System World To New
    
    *Warning Not Running Build*

--- src/main/java/de/butzlabben/world/util/MultiWorldUtil.java
+++ src/main/java/de/butzlabben/world/util/MultiWorldUtil.java
@@ -1,21 +1,97 @@
 package de.butzlabben.world.util;
 
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
+import de.butzlabben.world.config.DependenceConfig;
+import de.butzlabben.world.config.PluginConfig;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-public class MultiWorldUtil extends FileConfiguration {
-    @Override
-    public String saveToString() {
-        return null;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class MultiWorldUtil {
+    //Done
+    Dictionary playerWorldList = new Hashtable();
+
+    public static File getWorldList(String uuid) {
+        File worldList = new File(PluginConfig.getWorlddir()+ "/" + uuid  + "/worldlist.yml");
+        if (!worldList.exists()) {
+            worldList = new File(PluginConfig.getWorlddir() + "/" + uuid + "/worldlist.yml");
+        }
+        if (!worldList.exists()) {
+            worldList = new File(uuid  + "/worldlist.yml");
+        }
+        return worldList;
     }
 
-    @Override
-    public void loadFromString(String contents) throws InvalidConfigurationException {
 
+    public static void create(UUID uuid) {
+        DependenceConfig dc = new DependenceConfig(uuid);
+        String worldname = dc.getWorldname();
+        File file = new File(PluginConfig.getWorlddir() + uuid + "/worldlist.yml");
+        try {
+            file.createNewFile();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            System.err.println("Error while creating worldconfig for " + uuid.toString());
+        }
+
+        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+        Dictionary worlds = new Hashtable();
+        File dirc = new File(PluginConfig.getWorlddir() + uuid + "/");
+        String[] names = dirc.list();
+
+        for(String name : names)
+        {
+            if (new File(dirc + name).isDirectory())
+            {
+                worlds.put(name, new File(dirc + name));
+            }
+        }
+        cfg.set("WorldList", worlds);
+        try {
+            cfg.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error while saving worldconfig for " + uuid.toString());
+        }
     }
 
-    @Override
-    protected String buildHeader() {
-        return null;
+    public MultiWorldUtil save(UUID uuid) throws IOException {
+        File file = new File(PluginConfig.getWorlddir() + uuid + "/worldlist.yml");
+        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+
+        Dictionary worlds = new Hashtable();
+        File dirc = new File(PluginConfig.getWorlddir() + uuid + "/");
+        String[] names = dirc.list();
+
+        for(String name : names)
+        {
+            if (new File(dirc + name).isDirectory())
+            {
+                worlds.put(name, new File(dirc + name));
+            }
+        }
+        cfg.set("WorldList", worlds);
+
+        cfg.save(file);
+        return this;
     }
+
+
+    public MultiWorldUtil load(UUID uuid) {
+        File file = new File(PluginConfig.getWorlddir() + uuid + "/worldlist.yml");
+        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+        playerWorldList = (Dictionary) cfg.get("WorldList", "Unknown World");
+            try {
+                save(uuid);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return this;
+    }
+
+    public Dictionary getworlds() {
+        return playerWorldList;
+    }
+
 }
