7b459b49550eb1acbb4b507bb505c25b794a8265
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-04 16:24:56 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This is a test.sh be careful!
 # Release code changer to have it easy to build
-
 # Get config informations
 POM-RELEASE=$(cat ./workflows/release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
 RELEASEVER=$(cat ./workflows/release-code-changer.yml | grep release.version: | cut -d ':' -f2)
-
-sed -i "s:$DSERVERFOLDER:g" ./values.txt $>/dev/null 2>&1 WorldSystem/2.4.11/
+sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
+sed -i "0,:<version>.*</version>:s:<version>$POM-RELEASE</version>" ./../pom.xml $>/dev/null 2>&1
+exit 0
