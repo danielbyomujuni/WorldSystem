eaa3d2f66f8dcfcc293ebf5bbf9ba0f43853cf12
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-04 08:40:20 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -3,7 +3,7 @@
 # Release code changer to have it easy to build
 
 # Get config informations
-PRE-RELEASE=$(cat ./workflows/release-code-changer.yml | grep pre.release: | cut -d ':' -f2)
+POM-RELEASE=$(cat ./workflows/release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
 RELEASEVER=$(cat ./workflows/release-code-changer.yml | grep release.version: | cut -d ':' -f2)
 
-grep sed -i "s:$DSERVERFOLDER:g" ./values.txt $>/dev/null 2>&1 WorldSystem/2.4.11/
+sed -i "s:$DSERVERFOLDER:g" ./values.txt $>/dev/null 2>&1 WorldSystem/2.4.11/
