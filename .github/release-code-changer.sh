fc0b7bc85b7fd3963b4aac40eb60a3aa6c036ed4
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-05 08:29:04 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -5,12 +5,12 @@
 # Configuration
 
 # What plugin version should be in the pom.xml
-POM-RELEASE=2.4.20-SNAPSHOT
+POMRELEASE=2.4.20-SNAPSHOT
 
 # What version should be shown in the README.md latest badge
 RELEASEVER=2.4.11
 
 
 sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
-sed -i "0,:<version>.*</version>:s:<version>$POM-RELEASE</version>" ./../pom.xml $>/dev/null 2>&1
+sed -i "0,:<version>.*</version>:s:<version>$POMRELEASE</version>" ./../pom.xml $>/dev/null 2>&1
 exit 0
