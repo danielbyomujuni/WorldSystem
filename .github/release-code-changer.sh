cfd73c359a78380c97ec7e46fad211e66d352be5
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-05 08:23:54 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -2,6 +2,7 @@
 # This is a test.sh be careful!
 # Release code changer to have it easy to build
 # Get config informations
+apt-get install coreutils
 POM-RELEASE=$(cat ./release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
 RELEASEVER=$(cat ./release-code-changer.yml | grep release.version: | cut -d ':' -f2)
 sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
