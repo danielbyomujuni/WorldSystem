98ce079802633e8379a1cd4c02619754c0f25998
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-05 08:25:49 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -2,9 +2,8 @@
 # This is a test.sh be careful!
 # Release code changer to have it easy to build
 # Get config informations
-apt-get install coreutils
-POM-RELEASE=$(cat ./release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
-RELEASEVER=$(cat ./release-code-changer.yml | grep release.version: | cut -d ':' -f2)
+POM-RELEASE=2.4.20-SNAPSHOT
+RELEASEVER=2.4.11
 sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
 sed -i "0,:<version>.*</version>:s:<version>$POM-RELEASE</version>" ./../pom.xml $>/dev/null 2>&1
 exit 0
