14cea8f96d93ad7da89826c725cfa1c625f97c6e
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-05 07:59:28 +0200

    Changed the config location

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -2,8 +2,8 @@
 # This is a test.sh be careful!
 # Release code changer to have it easy to build
 # Get config informations
-POM-RELEASE=$(cat ./workflows/release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
-RELEASEVER=$(cat ./workflows/release-code-changer.yml | grep release.version: | cut -d ':' -f2)
+POM-RELEASE=$(cat ./release-code-changer.yml | grep pom.release: | cut -d ':' -f2)
+RELEASEVER=$(cat ./release-code-changer.yml | grep release.version: | cut -d ':' -f2)
 sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
 sed -i "0,:<version>.*</version>:s:<version>$POM-RELEASE</version>" ./../pom.xml $>/dev/null 2>&1
 exit 0
