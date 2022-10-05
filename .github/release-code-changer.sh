10bb6287eef9de5fe51e8265d480861f89e9d290
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-05 08:27:44 +0200

    Update release-code-changer.sh

--- .github/release-code-changer.sh
+++ .github/release-code-changer.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# This is a test.sh be careful!
-# Release code changer to have it easy to build
-# Get config informations
+# Hi, this is a release code changer to change code when you released a new version.
+# This changes the README.md and pom.xml at the moment.
+# Mabe i will add here more features or change them.
+# Configuration
+
+# What plugin version should be in the pom.xml
 POM-RELEASE=2.4.20-SNAPSHOT
+
+# What version should be shown in the README.md latest badge
 RELEASEVER=2.4.11
+
+
 sed -i "0,:WorldSystem/.*/:s:WorldSystem/$RELEASEVER/" ./README.md $>/dev/null 2>&1
 sed -i "0,:<version>.*</version>:s:<version>$POM-RELEASE</version>" ./../pom.xml $>/dev/null 2>&1
 exit 0
