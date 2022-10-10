effc3eeb963d79caccbab70366840c1101a00246
CrazyCloudCraft | Argantiu <95505222+CrazyCloudCraft@users.noreply.github.com>
2022-10-10 10:07:36 +0200

    Update CommandRegistry.java

--- src/main/java/de/butzlabben/world/command/CommandRegistry.java
+++ src/main/java/de/butzlabben/world/command/CommandRegistry.java
@@ -28,12 +28,12 @@ public class CommandRegistry implements CommandExecutor {
                         return ws.getCommand(sender, command, label, args);
                 case "gui":
                         return ws.guiCommand(sender, command, label, args);
-                case "confirm":
+                /*case "confirm":
                     if (sender.hasPermission("ws.confirm")) {
                         return ws.confirmCommand(sender, command, label, args);
                     } else {
                         return false;
-                    }
+                    }*/
                 case "home":
                     return ws.homeCommand(sender, command, label, args);
                 case "info":
