b364b3da74923ac787a3ec67733dc6b4e5fb9e0f
danielb <danielbyomujuni@nekosyndicate.com>
2024-05-23 17:00:02 +0000

    added abstract database and tests

--- src/main/java/de/butzlabben/WorldSystem/data/AbstractSqlLiteDatabase.java
+++ src/main/java/de/butzlabben/WorldSystem/data/AbstractSqlLiteDatabase.java
@@ -1,10 +1,37 @@
 package de.butzlabben.WorldSystem.data;
 
+import de.butzlabben.world.config.PluginConfig;
+
+import java.sql.*;
+
 public abstract class AbstractSqlLiteDatabase {
 
-    public AbstractSqlLiteDatabase() {
+    private Connection connection;
+    private Statement state;
 
+    public AbstractSqlLiteDatabase() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:" + PluginConfig.getSqliteFile());
+        this.construct_database();
     }
 
+    public AbstractSqlLiteDatabase(String db_path) throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:" + db_path);
+        this.state =  this.connection.createStatement();
+        this.construct_database();
+    }
 
+    protected ResultSet query(String sql) throws SQLException {
+        return this.state.executeQuery(sql);
+    }
+
+    protected abstract void construct_database();
+
+    public void close() {
+        try {
+            this.state.close();
+            this.connection.close();
+        } catch (SQLException e) {
+
+        }
+    }
 }
--- src/test/java/de/butzlabben/WorldSystem/data/MockDatabase.java
+++ src/test/java/de/butzlabben/WorldSystem/data/MockDatabase.java
@@ -1,4 +1,28 @@
 package de.butzlabben.WorldSystem.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MockDatabase extends AbstractSqlLiteDatabase {
+
+    public MockDatabase() throws SQLException {
+        super("./mock.db");
+    }
+
+    @Override
+    protected void construct_database() {
+        try {
+            this.query("CREATE TABLE IF NOT EXISTS RealTable (id INTEGER PRIMARY KEY, quantity INTEGER);").close();
+        } catch (SQLException e) {
+            //
+        }
+    }
+
+    public boolean does_table_exist(String Table_Name) throws SQLException {
+        ResultSet res = this.query(String.format("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = '%s';", Table_Name));
+        res.next();
+        int count = res.getInt("count(*)");
+        System.out.println(count);
+        return (count == 1);
+    }
 }
